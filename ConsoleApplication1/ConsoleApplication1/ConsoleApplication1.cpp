812eff8eb34527ec32afcf3441cdd20cf80f1eb4
rnrytjr2 <43197846+rnrytjr2@users.noreply.github.com>
2022-10-20 21:42:01 +0900

    solved

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#define collection cin.tie(NULL);ios_base::sync_with_stdio(false);
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    int K=0,answer=0;
+    stack<int> answers;
+
+    collection;
+    cin >> K;
+    for (int i = 0; i < K; i++)
+    {
+        int value;
+        cin >> value;
+        if (value != 0)
+        {
+            answers.push(value);
+        }
+        else
+        {
+            answers.pop();
+        }
+    }
+    for (int i = answers.size(); i > 0; i--)
+    {
+        answer += answers.top();
+        answers.pop();
+    }
+    cout << answer;
+}
+
